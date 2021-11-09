d4f70d2c2167e14c4aefc4c073cea560a173e8e4
Juan Camilo Jiménez Rojas <jcjimenezr@eafit.edu.co>
2021-11-08 19:49:52 -0500

    firebase

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAXiU1FiNJHBpxawY7SsblCfFV5dRPH7rk",
+  authDomain: "bolumbia-e8c44.firebaseapp.com",
+  databaseURL: "https://bolumbia-e8c44-default-rtdb.firebaseio.com",
+  projectId: "bolumbia-e8c44",
+  storageBucket: "bolumbia-e8c44.appspot.com",
+  messagingSenderId: "1075351382092",
+  appId: "1:1075351382092:web:7e377c267cf064d34a6ce9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
