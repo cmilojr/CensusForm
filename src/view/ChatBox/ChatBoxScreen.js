52468a6bc0b6646ea2d6d167494aff97dd3decfb
srodriguev <60025041+srodriguev@users.noreply.github.com>
2021-11-27 21:59:49 -0500

    return of auxiliary link buttons

--- src/view/ChatBox/ChatBoxScreen.js
+++ src/view/ChatBox/ChatBoxScreen.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { View, Text, ScrollView} from 'react-native'
 import BackButton from '../shared/BackButton'
 import {TextQuestion} from '../shared/QuestionsType'
+import { Button } from 'react-native-elements'
 
 const ChatBoxScreen = (props) => {
     const {setShowHD, showBack} = props
@@ -17,13 +18,22 @@ const ChatBoxScreen = (props) => {
                 fontWeight: 'bold',
                 fontSize: 32,
                 margin: 20
-            }}>Talk with Erika</Text>
+            }}> Talk with Erika </Text>
+            
             <ScrollView style={{
                 width: '100%',
             }}>
                 <View style={{
                     height: 150
                 }}/>
+
+                    <Button
+                        title="Back"
+                        onPress={() => props.navigation.goBack()}
+                        containerStyle={{ height: 40, marginTop: 20 }}
+                        fontColor="textBgPrimary"
+                    />
+
             </ScrollView>
             {
                 showBack && <BackButton setBackAction={setShowHD} />
